6865f4f6e4423eaede4e43fd1eed8971ec3b59dd
VictorHBellini <victorfebellini@gmail.com>
2023-03-09 20:13:44 -0300

    Exercícios

--- Exercícios_Treinar/Area.py
+++
@@ -1,7 +0,0 @@
-Altura = float(input('Entre com a Altura do Triangulo: '))
-Base = float(input('Entre com a Base do Triangulo: '))
-
-Multi = Altura * Base
-print(Multi)
-Area = Multi / 2
-print(Area)
---
+++ Exercícios_Treinar/Area_Triangulo.py
@@ -0,0 +1,11 @@
+# ____________Calculando a área do Triangulo____________ #
+Altura = float(input('Entre com a Altura do Triangulo: '))
+Base = float(input('Entre com a Base do Triangulo: '))
+# ______________________________________________________ #
+
+# ____________Multiplicação____________ #
+Multi = Altura * Base
+print(Multi)
+Area = Multi / 2
+print(Area)
+# _____________________________________ #
