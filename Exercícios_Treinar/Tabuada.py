3d6a387c0d921b955eceec5d6cffe11be8a78ba9
VictorHBellini <victorfebellini@gmail.com>
2023-03-10 19:58:28 -0300

    Exercícios Finalizados

--- Exercícios_Treinar/Tabuada.py
+++ Exercícios_Treinar/Tabuada.py
@@ -1,1 +1,13 @@
+# _____________________Tabuada feita em Python_____________________ #
+Tabuada = int(input("Insira o Número da Tabuada que você deseja: "))
+# _________________________________________________________________ #
 
+# Estrutura de Repetições - Loops #
+for i in range(0, 11):
+  print(Tabuada, "x", i, "=", Tabuada * i)
+
+# O for é uma váriavel utilizada para o uso de repetições. #
+# O in retorna True caso o valor seja encontrado na sequência. #
+# O range fornece uma sequência de números inteiros baseada nos argumentos da função. #
+
+# _______________________________ #
