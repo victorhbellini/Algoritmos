079daa739c5991befbc1bcb616d07432f624c4d6
VictorHBellini <victorfebellini@gmail.com>
2023-03-09 22:21:18 -0300

    Fototeta

---
+++ Exercícios_Treinar/Gambiarra.py
@@ -0,0 +1,24 @@
+# ________________Gambiarra________________ #
+Numero1 = int(input("Insira o 1º Número: "))
+Numero2 = int(input("Insira o 2º Número: "))
+# _________________________________________ #
+
+# ____O primeiro valor vezes o segundo valor____ #
+Valor = Numero1 * Numero2
+print(Valor)
+# ______________________________________________ #
+
+# A soma do 2ºNum com a metade do 1ºNum #
+Soma = Numero2 + (Numero1 / 2)
+print(Soma)
+# ______________________________________________ #
+
+# A diferença do 1ºNum com o 2ºNum #
+Diferença = Numero1 - Numero2
+print(Diferença)
+# ________________________________ #
+
+# O valor negativo do 2ºNum #
+Neg_Num2 = -1 * Numero2
+print(Neg_Num2)
+# ________________________________ #
